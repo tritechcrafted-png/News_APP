e7c51e83ac5690c55cfe2a0ff3b8872fe5471518
user <user1@aaa.com>
2026-06-15 10:08:21 +0900

    Day 1 debug

---
+++ News_APP/news/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Article
+
+#デコレーターを使うことで下のクラスをDjangoが扱えるようにする
+@admin.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    管理画面でArticleモデルを管理するための設定
+    """
+
+    #一覧に表示するカラム
+    list_display=("short_title_display", "source","article_date")
+
+    #list_filter:一覧の右側にフィルターを追加する
+    list_filter = ("source", "article_date")
+
+    #管理画面に検索バーを追加する
+    search_fields=("title", "description")
+
+    list_per_page=50
+
+    def short_title_display(self, obj):
+        """
+        管理画面の一覧でタイトルに表示する値を返す
+        Artcile モデルの short_title()を呼び出す
+        """
+
+        #obj = 現在の行のArticleのインスタンス
+        return obj.short_title()
+    
+    short_title_display.short_description = "タイトル"
+
+    
---
+++ News_APP/news/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from . import views
+
+#アプリの名前を指定することで、どこのアプリのviewを参照しているのか
+#わかりやすくすることができる
+app_name="news"
+
+urlpatterns=[
+
+    #最初のページ　ホームページ
+    path("", views.home, name="home"),
+
+    #urlのその日の記事を出していく
+    path("day/<int:year>/<int:month>/<int:day>/", views.day_articles, name="day"),
+
+    #更新ボタンのフォームを押したときに送信するurl
+    #@require_POSTでGETアクセスを拒否する
+    path("update/", views.update_feed, name="update_feed"),
+]
+
+
